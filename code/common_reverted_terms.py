908a7360d1a47db76a9ecb4c9790411abdc0a83d
Nate E TeBlunthuis <nathante@mox2.hyak.local>
2021-03-04 14:16:29 -0800

    finish making wordcloud

--- code/common_reverted_terms.py
+++ code/common_reverted_terms.py
@@ -9,7 +9,7 @@ from multiprocessing import Pool
 
 overwrite = False
 
-revcache_path = pathlib.Path("../data/climate_change_revs.pickle")
+revcache_path = pathlib.Path("data/climate_change_revs.pickle")
 
 def update_revs():
     api_session = api.Session("https://en.wikipedia.org/w/api.php")
@@ -39,7 +39,7 @@ if revcache_path.exists() and overwrite is False:
 else:
     all_revs = update_revs()
 
-checksum_revisions = [(rev.get('sha1',None),rev) for rev in all_revs]
+checksum_revisions = [(rev.get('sha1',None),rev) for rev in all_revs if 'sha1' in rev]
 
 reverts = mwreverts.detect(checksum_revisions,radius=15)
 
@@ -51,8 +51,13 @@ def find_reverted_tokens(revert):
     reverted_to = revert.reverted_to
     
     initial_text = initial_revision.get('*',None)
+    if initial_text is None:
+        print(initial_revision)
     initial_tokens = deltas.tokenizers.wikitext_split.tokenize(initial_text)
     to_tokens = deltas.tokenizers.wikitext_split.tokenize(reverted_to.get('*',None))
+
+    if not '*' in reverted_to:
+        print(reverted_to)
 
     diff_ops = deltas.segment_matcher.diff(initial_tokens,to_tokens)
 
@@ -63,13 +68,13 @@ def find_reverted_tokens_list(revert):
     return list(find_reverted_tokens(revert))
 
 
-pool = Pool(3)
+pool = Pool(27)
 
-all_deleted_tokens = list(pool.map(find_reverted_tokens_list, list(reverts)))
+all_deleted_tokens = list(map(find_reverted_tokens_list, list(reverts)[0:100]))
 
 all_deleted_tokens_ = Counter((t.lower() for t in chain(*all_deleted_tokens)))
 
-all_deleted_tokens_
+#all_deleted_tokens_
 
 from wordcloud import WordCloud, STOPWORDS
 
