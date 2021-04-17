dd459505ce19c5781d7443b19e8d0c2efbaa67ec
Nathan TeBlunthuis <nathante@uw.edu>
2021-04-17 10:22:12 -0700

    Take the proportion of the number of edges that goes to climate.

---
+++ code/count_edges.py
@@ -0,0 +1,35 @@
+from collections.abc import Iterable
+from pathlib import Path
+import pandas as pd
+import gzip
+import csv
+from multiprocessing import Pool
+
+def lines_from_gzip(path: Path) -> Iterable:
+    lines = gzip.open(path,'rt')
+    # throw away the the header
+    header = next(lines)
+    # convert to string
+#    lines = (s.decode() for s in lines)
+    print(path)
+    try:
+        yield from csv.reader(lines,dialect='excel-tab')
+    except csv.Error:
+        print(f"Error in {path}")
+
+def count_edges(year):
+    lines = lines_from_gzip(f"../data/enwiki.wikilink_graph.{year}-03-01.csv.gz")
+    count = 0
+    count_climate = 0 
+    targets = ['6266','5042951']
+    for line in lines:
+        if line[2] in targets:
+            count_climate = count_climate + 1
+        count = count + 1
+    return {'year':year,'N':count,'N_climate':count_climate}
+
+pool = Pool(4)
+rows  = list(pool.map(count_edges, (str(y) for y in range(2001,2004))))
+df = pd.DataFrame(rows)
+df['prop_climate'] = df.N / df.N_climate
+df.to_csv("../data/wikilink_counts.csv")
